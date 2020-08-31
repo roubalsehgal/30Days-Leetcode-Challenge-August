649a13a6e896770b5b5ddde7019276ba71d914de
roubalsehgal <roubalsehgal2000@gmail.com>
2020-08-31 21:11:35 +0530

    Day31 - Delete Node in a BST

---
+++ 31.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+	public TreeNode deleteNode(TreeNode root, int key) {
+		if (root == null) return root;
+		if (root.val == key) {
+			//check case 1
+			if (root.left == null && root.right == null) {
+				return null;
+			} else if (root.right == null || root.left == null) {
+				return root.right == null ? root.left : root.right;
+			} else {
+				root.val = inOrderSuccessor(root.right);
+				root.right = deleteNode(root.right, root.val);
+				return root;
+			}
+		}
+		if (root.val<key) {
+			root.right = deleteNode(root.right, key);
+		} else {
+			root.left = deleteNode(root.left, key);
+		}
+
+		return root;
+	}
+
+	private static int inOrderSuccessor(TreeNode root) {
+		if (root.left == null && root.right == null) return root.val;
+		return root.left != null ? inOrderSuccessor(root.left) : root.val;
+	}
+}
